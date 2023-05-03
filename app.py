58397038535e8d32af76f30311214858be26c405
BigBeefs <bigbeefs98@gmail.com>
2023-05-03 13:26:34 +0200

    Aggiornamento finito

--- app.py
+++ app.py
@@ -24,9 +24,31 @@ def main():
     df_pred=pd.DataFrame(data=list(zip(y_pred, y_test)),columns=['predicted', 'real'])
     df_pred['error'] = df_pred['predicted'] - df_pred['real']
     
-    tab1,tab2,tab3 = st.tabs(['Variazione di Prezzo','HeatMap Correlation Matrix','Error'])
-    
+    tab0,tab1,tab2,tab3 = st.tabs(['Modifica valori da predirre','Variazione di Prezzo','HeatMap Correlation Matrix','Error'])
+
+    with tab0:
+        modelup=mlem.api.load('model_.mlem')
+
+        crim = st.number_input('Insert a crim', min_value=1,max_value=500)
+        zn = st.number_input('Insert a zn',min_value=1,max_value=500)
+        indus = st.number_input('Insert a indus',min_value=1,max_value=500)
+        chas = st.number_input('Insert a chas',min_value=1,max_value=500) 
+        nox = st.number_input('Insert a nox',min_value=1,max_value=500)
+        rm = st.number_input('Insert a rm',min_value=1,max_value=500)
+        age = st.number_input('Insert a age',min_value=1,max_value=500)
+        dis = st.number_input('Insert a dis',min_value=1,max_value=500)
+        rad = st.number_input('Insert a rad',min_value=1,max_value=500)
+        tax = st.number_input('Insert a tax',min_value=1,max_value=500)
+        ptratio = st.number_input('Insert a ptratio',min_value=1,max_value=500)
+        b = st.number_input('Insert a b',min_value=1,max_value=500)
+        lstat = st.number_input('Insert a lstat',min_value=1,max_value=500)   
+
+        pred = modelup.predict([[crim, zn,indus, chas,nox,rm,age,dis,rad, tax,ptratio,b,lstat]]) [0]
+        st.write(round(pred,1))
+        
     with tab1:
+
+        
         fig=plt.figure(figsize=(18,10))
         sns.histplot(data=df['price']) 
         #streamlit ha problemi nel renderizzare il pairplot
@@ -51,5 +73,6 @@ def main():
         plt.legend(loc=2)
         st.write('Plot degli errori')
         st.pyplot(figureError)
+        
 if __name__ == "__main__":
     main()
